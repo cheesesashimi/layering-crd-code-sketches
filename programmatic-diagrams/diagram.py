7a82d55adad0f905a1f0cc849d31c26e2a15e40e
Zack Zlotnik <zzlotnik@redhat.com>
2022-08-01 10:46:58 -0400

    added diagrams for multiarch scenarios

--- programmatic-diagrams/diagram.py
+++ programmatic-diagrams/diagram.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
-from diagrams import Cluster, Diagram, Edge
+from diagrams import Cluster, Diagram, Edge, Node
 from diagrams.custom import Custom
 from diagrams.onprem import ci, cd
 from diagrams.onprem.compute import Server
-from diagrams.oci.compute import Functions, OCIRegistry, OKE
+from diagrams.oci.compute import Container, Functions, OCIRegistry, OKE
 from diagrams.generic.device import Tablet
 from diagrams.k8s.infra import Node as K8sNode
 from diagrams.k8s.infra import Master as K8sControl
 from diagrams.k8s.group import Namespace
 from diagrams.k8s.compute import Pod, DaemonSet
 from diagrams.onprem.vcs import Git
+
+def ARM64(text=""):
+    return Custom(text, "./Arm-logo-blue-pms313.svg")
+
+def AMD64(text=""):
+    return Custom(text, "./amd64_logo.svg")
+
+def POWER(text=""):
+    return Node("POWER " + text)
 
 def QuayRegistry(text=""):
     return Custom(text, "./Logo-Red_Hat-Quay.io-A-Standard-RGB.png")
@@ -63,3 +72,62 @@ with Diagram("Final Image Build Flow", show=False):
             for worker_node in worker_nodes:
                 mcd_pod >> Edge(label="$ rpm-ostree rebase") >> worker_node
                 worker_node << Edge(label="Image Pull") << incluster_registry
+
+with Diagram("Multiarch Scenario 1", show=False):
+    with Cluster("OpenShift Cluster"):
+        base_image_manifest_list = Node("Base Image Manifest List")
+        amd64_manifest = AMD64("AMD 64 Manifest")
+        master_mcp = Container("master\nMCP / Layered Image")
+        worker_mcp = Container("worker\nMCP / Layered Image")
+        amd64_control_plane = K8sControl("AMD64")
+        amd64_workers = K8sNode("AMD64 Workers\n(worker)")
+
+        base_image_manifest_list >> amd64_manifest >> [master_mcp, worker_mcp]
+        master_mcp >> amd64_control_plane
+        worker_mcp >> amd64_workers
+
+with Diagram("Multiarch Scenario 2", show=False):
+    with Cluster("OpenShift Cluster"):
+        base_image_manifest_list = Node("Base Image Manifest List")
+        amd64_manifest = AMD64("AMD 64 Manifest")
+        arm64_manifest = ARM64("ARM 64 Manifest")
+        master_mcp = Container("master\nMCP / Layered Image")
+        worker_mcp = Container("worker\nMCP / Layered Image")
+        amd64_control_plane = K8sControl("AMD64")
+        arm64_workers = K8sNode("ARM64 Workers\n(worker)")
+
+        base_image_manifest_list >> amd64_manifest >> master_mcp >> amd64_control_plane
+        base_image_manifest_list >> arm64_manifest >> worker_mcp >> arm64_workers
+
+with Diagram("Multiarch Scenario 3", show=False):
+    with Cluster("OpenShift Cluster"):
+        base_image_manifest_list = Node("Base Image Manifest List")
+        amd64_manifest = AMD64("AMD 64 Manifest")
+        arm64_manifest = ARM64("ARM 64 Manifest")
+        master_mcp = Container("Master MCP / Layered Image")
+        arm64_worker_mcp = Container("worker-arm64\nWorker MCP / Layered Image")
+        amd64_worker_mcp = Container("worker-amd64\nWorker MCP / Layered Image")
+        amd64_control_plane = K8sControl("AMD64")
+        arm64_workers = K8sNode("ARM64 Workers\n(worker-arm64)")
+        amd64_workers = K8sNode("AMD64 Workers\n(worker-amd64)")
+
+        base_image_manifest_list >> amd64_manifest >> master_mcp >> amd64_control_plane
+        amd64_manifest >> amd64_worker_mcp >> amd64_workers
+        base_image_manifest_list >> arm64_manifest >> arm64_worker_mcp >> arm64_workers
+
+with Diagram("Multiarch Scenario 4", show=False):
+    with Cluster("OpenShift Cluster"):
+        base_image_manifest_list = Node("Base Image Manifest List")
+        amd64_manifest = AMD64("AMD 64 Manifest")
+        power_manifest = POWER("POWER Manifest")
+        master_mcp = Container("Master MCP / Layered Image")
+        streaming_worker_mcp = Container("worker-power-streaming\nWorker MCP / Layered Image")
+        transactional_worker_mcp = Container("worker-power-transactional\nWorker MCP / Layered Image")
+        amd64_control_plane = K8sControl("AMD64")
+        streaming_workers = K8sNode("POWER Workers\n(worker-power-streaming)")
+        transactional_workers = K8sNode("AMD64 Workers\n(worker-power-transactional)")
+
+        base_image_manifest_list >> amd64_manifest >> master_mcp >> amd64_control_plane
+        base_image_manifest_list >> power_manifest >> [streaming_worker_mcp, transactional_worker_mcp]
+        streaming_worker_mcp >> streaming_workers
+        transactional_worker_mcp >> transactional_workers
